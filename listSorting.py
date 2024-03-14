6a2bb3b69cbb01a691d9f14c6b2936c62b07a9b9
Lucas Jackson <futurelucas123@gmail.com>
2024-03-14 19:36:16 +0000

    almost done

--- listSorting.py
+++ listSorting.py
@@ -7,21 +7,60 @@ def listCreator(para):
     while listLength != len(unsortedList):
         unsortedList.append(input("What is another thing you want in your list?"))
 
-    return unsortedList
-
 def listSorter():
+    def indexer(index):
+        tempIndex = unsortedList.index(tempThing) + 1
+        sortedList.insert(index, tempThing)
+        print(sortedList)
+        sortedList.pop(tempIndex)
+        print(sortedList)
+    #def correcting(index):
+        #for x in unsortedList:
+         #   if x == tempThing:
+          #      break
+        #while x != tempThing:
+         #   print("The thing you entered is not in your list, try again.")
+          #  tempThing = input("What is your favorite thing in your list? (Make sure capitalizations are the same)")
+           # for y in unsortedList:
+            #    if y == tempThing:
+             #       tempIndex = unsortedList.index(tempThing) + 1
+              #      sortedList.insert(index, tempThing)
+               #     print(sortedList)
+                #    sortedList.pop(tempIndex)
+                 #   print(sortedList)
+                  #  x = tempThing
     sortedList = unsortedList
     if listLength > 1:
-        print(unsortedList)
-        favItem = input("What is your favorite thing that is in your list? (Make sure capitalizations are the same)")
+        tempThing = input("What is your favorite thing in your list? (Make sure capitalizations are the same)")
         for x in unsortedList:
-            if x == favItem:
-                favIndex = unsortedList.index(favItem) + 1
-                sortedList.insert(0, favItem)
-                sortedList.pop(favIndex)
+            if x == tempThing:
+                indexer(0)
                 break
-        if x != favItem:
-            print("uh oh")
+        #correcting(0)
+        while x != tempThing:
+            print("The thing you entered is not in your list, try again.")
+            tempThing = input("What is your favorite thing in your list? (Make sure capitalizations are the same)")
+            for y in unsortedList:
+                if y == tempThing:
+                    tempIndex = unsortedList.index(tempThing) + 1
+                    sortedList.insert(0, tempThing)
+                    print(sortedList)
+                    sortedList.pop(tempIndex)
+                    print(sortedList)
+                    x = tempThing
+        if listLength > 2:
+            thingsAdded = 0
+            for z in unsortedList:
+                thingsToBeAdded = len(unsortedList) - 2
+                if thingsAdded < thingsToBeAdded:
+                    tempThing = input("What is your next favorite thing in your list? (Make sure capitalizations are the same)")
+                    indexer(thingsAdded + 1)
+                    thingsAdded = thingsAdded + 1
+                else:
+                    print("Here is your ordered list: " + str(sortedList))
+                    break
+        else:
+            print("Here is your ordered list: " + str(sortedList))
     else:
         print("Here is your ordered list: " + str(sortedList))
 
