0d24c4fcfcba1b1506d33800e1000f83ac9f9b78
Lucas Jackson <futurelucas123@gmail.com>
2024-03-15 18:20:10 +0000

    finished

--- listSorting.py
+++ listSorting.py
@@ -11,24 +11,7 @@ def listSorter():
     def indexer(index):
         tempIndex = unsortedList.index(tempThing) + 1
         sortedList.insert(index, tempThing)
-        print(sortedList)
         sortedList.pop(tempIndex)
-        print(sortedList)
-    #def correcting(index):
-        #for x in unsortedList:
-         #   if x == tempThing:
-          #      break
-        #while x != tempThing:
-         #   print("The thing you entered is not in your list, try again.")
-          #  tempThing = input("What is your favorite thing in your list? (Make sure capitalizations are the same)")
-           # for y in unsortedList:
-            #    if y == tempThing:
-             #       tempIndex = unsortedList.index(tempThing) + 1
-              #      sortedList.insert(index, tempThing)
-               #     print(sortedList)
-                #    sortedList.pop(tempIndex)
-                 #   print(sortedList)
-                  #  x = tempThing
     sortedList = unsortedList
     if listLength > 1:
         tempThing = input("What is your favorite thing in your list? (Make sure capitalizations are the same)")
@@ -36,18 +19,6 @@ def listSorter():
             if x == tempThing:
                 indexer(0)
                 break
-        #correcting(0)
-        while x != tempThing:
-            print("The thing you entered is not in your list, try again.")
-            tempThing = input("What is your favorite thing in your list? (Make sure capitalizations are the same)")
-            for y in unsortedList:
-                if y == tempThing:
-                    tempIndex = unsortedList.index(tempThing) + 1
-                    sortedList.insert(0, tempThing)
-                    print(sortedList)
-                    sortedList.pop(tempIndex)
-                    print(sortedList)
-                    x = tempThing
         if listLength > 2:
             thingsAdded = 0
             for z in unsortedList:
